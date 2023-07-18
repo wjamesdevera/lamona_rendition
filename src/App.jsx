eda4ac12bf925dfe0643666f456a5c98320f9a3b
Kody <w.jamesdevera@gmail.com>
2023-07-18 13:37:41 +0800

    fixed navbar issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Footer, Navbar } from './components'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import { Home, About, Directory, Favorites, Choose } from './pages'
 
 const App = () => (
-  <div className='flex flex-col bg-bg-color text-text-body font-rubik overflow-hidden h-screen'>
-    <Navbar/>
-    <div className="bg-bg-color grow">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-      </Routes>
+  <Router>
+    <div className='flex flex-col bg-bg-color text-text-body font-rubik overflow-hidden h-screen'>
+      <Navbar/>
+      <div className="bg-bg-color grow">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </div>
+      <Footer className='shrink-0'></Footer>
     </div>
-    <Footer className='shrink-0'></Footer>
-  </div>
+  </Router>
 )
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    const location = useLocation();
     const isActive = (path) => {
-        return location.pathname === path ? 'text-main' : '';
+        return location.pathname === path ? 'text-main' : 'text-[#787877]';
     }
   return (
     <header className='sticky z-10 left-0 bg-bg-color'>
@@ -14,29 +15,29 @@ const Navbar = () => {
             <div className="">
                 <ul className='xl:flex hidden gap-[90px] text-xl font-raleway font-extrabold text-[#787877]'>
                     <li>
-                        <a href="/" className={`hover:text-main ${isActive('/')}`}>
+                        <NavLink to="/" className={`hover:text-main ${isActive('/')}`}>
                             HOME 
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="/about" className={`hover:text-main ${isActive('/about')}`}>
+                        <NavLink to="/about" className={`hover:text-main ${isActive('/about')}`}>
                             ABOUT
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="/directory" className={`hover:text-main ${isActive('/directory')}`}>
+                        <NavLink to="/directory" className={`hover:text-main ${isActive('/directory')}`}>
                             DIRECTORY
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="/favorites" className={`hover:text-main ${isActive('/favorites')}`}>
+                        <NavLink to="/favorites" className={`hover:text-main ${isActive('/favorites')}`}>
                             FAVORITES
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="/choose" className={`hover:text-main ${isActive('/choose')}`}>
+                        <NavLink to="/choose" className={`hover:text-main ${isActive('/choose')}`}>
                             CAN'T DECIDE?
-                        </a>
+                        </NavLink>
                     </li>
                 </ul>
             </div>
